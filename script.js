c29e7851087997e668ca72929a87f361ac790cda
louisdavenicario <nicario.louis@gmail.com>
2025-03-14 23:04:52 +0800

    adding back the old filter

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
         finalCanvas.width = FRAME_WIDTH;
         finalCanvas.height = FRAME_HEIGHT;
 
-        ctx.filter = "grayscale(99%) contrast(120%) brightness(95%) sepia(20%) blur(1px)";
+        ctx.filter = "grayscale(1) contrast(1.4) brightness(1.1) sepia(0.1)";
 
         let imagesLoaded = 0;
 
